835c94ae43a8bbc4b388856ac121df7b9daa66f3
GE-P <gerhard.eibl@kelenntech.com>
2023-10-11 17:10:57 +0200

    feat: added git prefix script + installer for windows ​

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# Git_Prefix
-A simple script that show the various commits prefixe's of a determined Git convention
+#
+![Alt text](docs/GP.png)                                                                  
+#
+
+## - Before proceding to the installation (Windows) :
+
+First you need to have the right to execute powershell script. To do that you need to open a powershell terminal as administrator and execute the following command :
+ #### - ``` Set-ExecutionPolicy Unrestricted ```
+
+To verify the good behavior of the command execute the command :
+#### - ``` Get-ExecutionPolicy ```
+
+After executing the scripts provided you'll need to put the execution policy back to restricted to avoid any security breach comming from scripts execution. Simply execute :
+#### - ``` Set-ExecutionPolicy Restricted ``` 
+
+#
+## - How to install the file :
+
+
+Simply clone the project. All the necessary files are already present.
+
+### Windows :
+#
+
+Open a powershell terminal as administrator and enter inside the folder containing the scripts, then simply execute as follow : 
+#### - ``` .\git_prefix_installer.ps1 ```
---
+++ src/git_prefix_installer.ps1
@@ -0,0 +1,36 @@
+﻿$RED = [ConsoleColor]::Red
+$GREEN = [ConsoleColor]::Green
+$BLUE = [ConsoleColor]::Blue
+$ENDCOLOR = [ConsoleColor]::White
+
+
+Write-Host "   _____ _ _        _____           __ _        _           _        _ _"           
+Write-Host "  / ____(_) |      |  __ \         / _(_)      (_)         | |      | | |"          
+Write-Host " | |  __ _| |_ ____| |__) | __ ___| |_ ___  __  _ _ __  ___| |_ __ _| | | ___ _ __" 
+Write-Host " | | |_ | | __|____|  ___/ '__/ _ \  _| \ \/ / | | '_ \/ __| __/ _` | | |/ _ \ '__|"
+Write-Host " | |__| | | |_     | |   | | |  __/ | | |>  <  | | | | \__ \ || (_| | | |  __/ |"
+Write-Host "  \_____|_|\__|    |_|   |_|  \___|_| |_/_/\_\ |_|_| |_|___/\__\__,_|_|_|\___|_|      /v1.0.0/"   
+                                                                                     
+                                                                                     
+
+$isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+if (-not $isAdmin) {
+    Write-Host ""
+    Write-Host "----> Is user administrator: " -NoNewline
+    Write-Host "no" -ForegroundColor Red
+    Write-Host "Please run this script as an administrator." -ForegroundColor Red
+    exit
+} else {
+    Write-Host ""
+    Write-Host "----> Is user administrator: " -NoNewline
+    Write-Host "yes" -ForegroundColor Green
+}
+
+$sourceFilePath = Join-Path -Path $PSScriptRoot -ChildPath "git-prefix"
+$destinationPath = "C:\Program Files\Git\mingw64\libexec\git-core\git-prefix"
+
+Copy-Item -Path $sourceFilePath -Destination $destinationPath -Force
+
+Write-Host ""
+Write-Host "----> Custom file 'git-prefix' installed successfully to: $destinationPath"
+Write-Host ""
